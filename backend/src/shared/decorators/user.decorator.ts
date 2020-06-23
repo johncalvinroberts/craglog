ca48357717f8b786dd206aea6874a6294e6feb28
John Roberts <john.calvin.roberts@gmail.com>
2020-06-24 00:20:36 +0800

    fix: user decorator

--- backend/src/shared/decorators/user.decorator.ts
+++ backend/src/shared/decorators/user.decorator.ts
@@ -1,7 +1,8 @@
-import { createParamDecorator } from '@nestjs/common';
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-export const User = createParamDecorator((data, req) => {
-  if (!!req.user) {
-    return !!data ? req.user[data] : req.user;
-  }
-});
+export const User = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    return request.user;
+  },
+);
