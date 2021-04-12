4740ddd7061dba26840e48233c4d43d730580635
John Roberts <john.calvin.roberts@gmail.com>
2021-04-13 00:24:22 +0800

    feat: docker images

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,25 @@
-set -ev
+set -e
 echo "Deploying craglog"
 
+# parse command line args
+while getopts t:a:f: flag
+do
+    case "${flag}" in
+        t) DOCKER_TAG=${OPTARG};;
+    esac
+done
+
+DOCKER_IMAGE="ghcr.io/johncalvinroberts/craglog:$DOCKER_TAG"
+
+
+# set gh container registry personal access token and login
+echo $CR_PAT | docker login ghcr.io -u USERNAME --password-stdin
+
+docker pull $DOCKER_IMAGE
+docker run $DOCKER_IMAGE -p 3001:3000
+docker run $DOCKER_IMAGE -p 3000:3000
+
+# reload caddy
 curl -X POST "http://localhost:2019/load" \
   -H "Content-Type: text/caddyfile" \
   --data-binary @Caddyfile
